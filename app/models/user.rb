907ec415ea8e8e07977676cb19c88afb53ec28d2
Alfredo <jalfredo.gallegos@gmail.com>
2026-03-28 13:03:18 -0700

    adds validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { in: 1..30 }
+  has_many :requests
 end
